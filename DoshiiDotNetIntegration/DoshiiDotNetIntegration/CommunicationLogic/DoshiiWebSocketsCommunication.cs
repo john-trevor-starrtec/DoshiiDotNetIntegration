d928b62d2b219cd7e2335b949dd5f768d2dda6d0
Liam P Clancy <liam.c@impos.com.au>
2014-08-15 14:29:54 +1000

    basic web sockets communication is working correctly with initial testing, sending and receiving messages.
    Signed-off-by: Liam P Clancy <liam.c@impos.com.au>

--- DoshiiDotNetIntegration/DoshiiDotNetIntegration/CommunicationLogic/DoshiiWebSocketsCommunication.cs
+++ DoshiiDotNetIntegration/DoshiiDotNetIntegration/CommunicationLogic/DoshiiWebSocketsCommunication.cs
@@ -2,10 +2,95 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using WebSocketSharp;
 
 namespace DoshiiDotNetIntegration.CommunicationLogic
 {
-    internal class DoshiiWebSocketsCommunication
+    /// <summary>
+    /// faciliates the web sockets communication with the doshii application
+    /// </summary>
+    internal class DoshiiWebSocketsCommunication : LoggingBase
     {
+
+        #region properties
+
+        /// <summary>
+        /// web socket object that will handle all the communications with doshii
+        /// </summary>
+        WebSocket ws = null;
+
+        #endregion
+
+        #region internal methods and events
+
+        #region events
+
+        internal event EventHandler<MessageEventArgs> webSocketMessage;
+
+        #endregion
+
+        #region methods
+
+        internal DoshiiWebSocketsCommunication(string webSocketURL)
+        {
+            ws = new WebSocket(webSocketURL);
+            ws.OnOpen += new EventHandler(ws_OnOpen);
+            ws.OnClose += new EventHandler<CloseEventArgs>(ws_OnClose);
+            ws.OnMessage += new EventHandler<MessageEventArgs>(ws_OnMessage);
+            ws.OnError += new EventHandler<ErrorEventArgs>(ws_OnError);
+            
+        }
+
+        internal void Connect()
+        {
+            if (ws != null)
+            {
+                ws.Connect();
+            }
+            else
+            {
+                log.Error(string.Format("Attempted to open a web socket connection before initializing the ws object"));
+            }
+        }
+
+        internal void SendMessage(string message)
+        {
+            log.Debug(string.Format("sending websockets message {0} to {1}", message, ws.Url.ToString()));
+            ws.Send(message);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region event handlers
+
+        private void ws_OnError(object sender, ErrorEventArgs e)
+        {
+            log.Error(string.Format("there was an error with the websockets connection to {0} the error was", ws.Url.ToString(), e.Message));
+        }
+
+        private void ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            OnMessageReceived(e);
+        }
+
+        private void OnMessageReceived(MessageEventArgs e)
+        {
+            webSocketMessage(this, e);
+        }
+
+        private void ws_OnClose(object sender, CloseEventArgs e)
+        {
+            log.Debug(string.Format("webScokets connection to {0} closed", ws.Url.ToString()));
+        }
+
+        private void ws_OnOpen(object sender, EventArgs e)
+        {
+            log.Debug(string.Format("webScokets connection open to {0}", ws.Url.ToString()));
+        }
+
+        #endregion
+
     }
 }
---
+++ DoshiiDotNetIntegration/DoshiiTesting/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DoshiiDotNetIntegration;
+using WebSocketSharp;
+
+
+namespace DoshiiTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //DoshiiWebSocketsCommunication wsLogic;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //wsLogic = new DoshiiWebSocketsCommunication(textBox1.Text);
+            //wsLogic.webSocketMessage += new EventHandler<WebSocketSharp.MessageEventArgs>(wsLogic_webSocketMessage);
+        }
+
+        void wsLogic_webSocketMessage(object sender, WebSocketSharp.MessageEventArgs e)
+        {
+            if (e.Type == Opcode.Text) {
+              // Do something with e.Data
+                textBox1.Text = e.Data.ToString();
+
+              return;
+            }
+
+            if (e.Type == Opcode.Binary) {
+              // Do something with e.RawData
+                textBox1.Text = e.RawData.ToString();
+
+              return;
+            }
+            
+            
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            //wsLogic.SendMessage(textBox1.Text);
+            textBox1.Text = "";
+        }
+    }
+}
