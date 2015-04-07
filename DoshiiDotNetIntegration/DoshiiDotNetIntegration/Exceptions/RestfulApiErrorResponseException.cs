577d80fab19f1e5e4a22293e7b643a88ce8287c5
Liam P Clancy <liam.c@impos.com.au>
2015-04-07 11:40:20 +1000

    a status code is now added to the exception thrown from the makeRequest method so it can be handled better by the calling code.
    Signed-off-by: Liam P Clancy <liam.c@impos.com.au>

--- DoshiiDotNetIntegration/DoshiiDotNetIntegration/Exceptions/RestfulApiErrorResponseException.cs
+++ DoshiiDotNetIntegration/DoshiiDotNetIntegration/Exceptions/RestfulApiErrorResponseException.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 
 namespace DoshiiDotNetIntegration.Exceptions
 {
     public class RestfulApiErrorResponseException : Exception
     {
+        public HttpStatusCode StatusCode { get; set; }
+        
         public RestfulApiErrorResponseException() : base() { }
+        public RestfulApiErrorResponseException(HttpStatusCode code) : base() { StatusCode = code; }
         public RestfulApiErrorResponseException(string message) : base(message) { }
         public RestfulApiErrorResponseException(string message, Exception ex) : base(message, ex) { }
     }
