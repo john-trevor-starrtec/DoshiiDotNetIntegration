263c5ac51a0bce5f491fd8b71a82bab3bc8a7ae3
Liam P Clancy <learnplaycreate@outlook.com>
2016-04-07 08:43:35 +1000

    some version fixes

--- DoshiiDotNetIntegration/DoshiiDotNetIntegration/Models/Location.cs
+++ DoshiiDotNetIntegration/DoshiiDotNetIntegration/Models/Location.cs
@@ -14,51 +14,68 @@ namespace DoshiiDotNetIntegration.Models
         /// <summary>
         /// the DoshiiId for the venue - give this value to partners to allow them to send orders and payments to your venue. 
         /// </summary>
-        string DoshiiId { get; set; }
+        public string DoshiiId { get; set; }
 
         /// <summary>
         /// the name of the venue
         /// </summary>
-        string Name { get; set; }
+        public string Name { get; set; }
 
         /// <summary>
         /// address of the venue
         /// </summary>
-        string AddressLine1 { get; set; }
+        public string AddressLine1 { get; set; }
 
         /// <summary>
         /// address of the venue
         /// </summary>
-        string AddressLine2 { get; set; }
+        public string AddressLine2 { get; set; }
 
         /// <summary>
         /// the city element of the venue address
         /// </summary>
-        string City { get; set; }
+        public string City { get; set; }
 
         /// <summary>
         /// the state element of the venue address
         /// </summary>
-        string State { get; set; }
+        public string State { get; set; }
 
         /// <summary>
         /// the postal code of the venue
         /// </summary>
-        string PostalCode { get; set; }
+        public string PostalCode { get; set; }
 
         /// <summary>
         /// the country element of the venue address
         /// </summary>
-        string Country { get; set; }
+        public string Country { get; set; }
 
         /// <summary>
         /// the phone number of the venue
         /// </summary>
-        string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
 
         /// <summary>
         /// the last time the venue was disconnected - will be null if the venue is connected. 
         /// </summary>
-        DateTime? DisconnectedDate { get; set; }
+        public DateTime? DisconnectedDate { get; set; }
+
+        /// <summary>
+        /// Resets all property values to default settings.
+        /// </summary>
+        public void Clear()
+        {
+            DoshiiId = String.Empty;
+            Name = String.Empty;
+            AddressLine1 = String.Empty;
+            AddressLine2 = String.Empty;
+            City = String.Empty;
+            State = String.Empty;
+            PostalCode = String.Empty;
+            Country = String.Empty;
+            PhoneNumber = String.Empty;
+            DisconnectedDate = null;
+        }
     }
 }
