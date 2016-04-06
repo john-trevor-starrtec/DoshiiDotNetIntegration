c2bfcae76e9e925acf7c85fbac1e94db687c103a
Liam P Clancy <learnplaycreate@outlook.com>
2016-04-06 13:44:49 +1000

    there are now namespace docs

---
+++ DoshiiDotNetIntegration/DoshiiDotNetIntegration/Interfaces/NamespaceDoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoshiiDotNetIntegration.Interfaces
+{
+    /// <summary>
+    /// provides a set of interfaces that must be implemented by the pos to instantiate <see cref="DoshiiManager"/>
+    /// <para/>These interfaces provide callback mechanism that will be called by the SDK to assist the implementation with the Doshii API
+    /// </summary>
+    internal class NamespaceDoc
+    {
+    }
+}
---
+++ DoshiiDotNetIntegration/DoshiiDotNetIntegration/NamespaceDoc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoshiiDotNetIntegration
+{
+    /// <summary>
+    /// <see cref="DoshiiDotNetIntegration"/> is an SDK developed to assist Point Of Sales software to integration with the Doshii API.
+    /// <para/>The SDK targets .NET 4.5
+    /// <para/>The SDK manages socket and HTTPs connections to the Doshii API
+    /// <para/><see cref="DoshiiManager"/> provides methods for the pos to update orders and transactions on the Doshii API.
+    /// <para/><see cref="DoshiiDotNetIntegration.Interfaces"/> provides call back mechanisms that alert the pos to new and modified orders, transactions, and logging (allowing the pos to implement SDK logging in their preferred method.
+    /// <para/> and callback mechanism to update and retrieve order versions, transaction versions, and checkinIds related to orders. 
+    /// <para/><see cref="DoshiiDotNetIntegration.Exceptions"/> provides a number of exceptions that are raised by the SDK and exceptions that must be raised by the pos under certain circumstances to correct operation of the SDK.
+    /// <para/><see cref="DoshiiDotNetIntegration.Models"/> provides the models used by the SDK while integration with the Doshii API.
+    /// <para/><see cref="DoshiiDotNetIntegration.Enums"/> provides the enums used by the SDK.
+    /// </summary>
+    internal class NamespaceDoc
+    {
+
+    }
+}
