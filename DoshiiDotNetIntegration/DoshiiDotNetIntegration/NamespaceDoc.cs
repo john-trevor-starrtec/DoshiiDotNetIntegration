452632779c34c2cf8f1d139c760af74b6021ec47
Liam P Clancy <learnplaycreate@outlook.com>
2016-04-06 16:06:37 +1000

    updated and fixed the build for stable release on buget

--- DoshiiDotNetIntegration/DoshiiDotNetIntegration/NamespaceDoc.cs
+++ DoshiiDotNetIntegration/DoshiiDotNetIntegration/NamespaceDoc.cs
@@ -16,6 +16,12 @@ namespace DoshiiDotNetIntegration
     /// <para/><see cref="DoshiiDotNetIntegration.Exceptions"/> provides a number of exceptions that are raised by the SDK and exceptions that must be raised by the pos under certain circumstances to correct operation of the SDK.
     /// <para/><see cref="DoshiiDotNetIntegration.Models"/> provides the models used by the SDK while integration with the Doshii API.
     /// <para/><see cref="DoshiiDotNetIntegration.Enums"/> provides the enums used by the SDK.
+    /// <h1 class="Heading">Known issue with Visual Studio</h1>
+    /// <para>If you have used the Nuget package manager to manage this SDK visual studio fails to automatically copy the 'websocketsharp.dll' into your build directories during build</para>
+    /// <h2 class="heading">Solution</h2>
+    /// <para>The solution to this issue is to directly reference the 'websocket-sharp.dll' in the project that is consuming the SDK.</para>
+    /// <para>The 'websocket-sharp.dll' can be found in the packages folder packages\DoshiiPosDotNetSDK.~currentVersion~\lib</para>
+    /// <para>The above file must be referenced and set to copy local in your project.</para>
     /// </summary>
     internal class NamespaceDoc
     {
