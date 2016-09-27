cff1ab5c9c051323e1dfdb8b6edb2a1c4fece355
Liam P Clancy <learnplaycreate@outlook.com>
2016-09-27 16:02:46 +1000

    modifying while implementing

--- DoshiiDotNetIntegration/DoshiiDotNetIntegration/Models/Member.cs
+++ DoshiiDotNetIntegration/DoshiiDotNetIntegration/Models/Member.cs
@@ -9,6 +9,11 @@ namespace DoshiiDotNetIntegration.Models
 {
     public class Member
     {
+        public Member()
+        {
+            Address = new Address();
+        }
+        
         public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
